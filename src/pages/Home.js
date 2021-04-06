f720a4739974dd48e6df409988a6598271f8db0b
ivegoie <axos.music@gmail.com>
2021-04-06 11:42:19 +0200

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "@material-ui/core";
+
+const Home = () => {
+  return (
+    <Container maxWidth="lg">
+      <h1>HomePage</h1>
+    </Container>
+  );
+};
+
+export default Home;
